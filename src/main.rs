57ca4db0aaca0dbd2f784633dd06238f0ee7052e
irieryota <ryotairie123@gmail.com>
2019-01-05 04:52:44 +0900

    とりあえず高階関数が解けるようになった

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use crate::evalml3::Fun;
 
 fn main() {
     //let exp = Exp::Let(Let::new("y", Exp::Int(2),Exp::Fun(Fun::new("x", Exp::Op(Op::Plus,box Exp::Var("x"),box Exp::Var("y"))))));
-    let exp = Exp::Let(
+    /*let exp = Exp::Let(
                 Let::new("sq", 
                     Exp::Fun(
                         Fun::new(
@@ -33,6 +33,33 @@ fn main() {
                             box Exp::Int(4)
                         )
                     )        
+                )
+            );*/
+    let exp = Exp::Let(
+                Let::new("sm", 
+                    Exp::Fun(
+                        Fun::new(
+                            "f", 
+                            Exp::Op(
+                                Op::Plus,
+                                box Exp::App(box Exp::Var("f"),box Exp::Int(3)),
+                                box Exp::App(box Exp::Var("f"),box Exp::Int(4)),
+                            )
+                        )
+                    ),
+                    Exp::App(
+                        box Exp::Var("sm"),
+                        box Exp::Fun(
+                            Fun::new(
+                                "x", 
+                                Exp::Op(
+                                    Op::Times,
+                                    box Exp::Var("x"),
+                                    box Exp::Var("x"),
+                                )
+                            )
+                        ),
+                    ),       
                 )
             );
     let r = exp.solve(&Env::None);
